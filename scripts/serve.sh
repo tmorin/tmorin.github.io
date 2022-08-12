4e46ee9dda99efacd12333f24838e7ff3e750f85
tmorin <thibault@morin.io>
2022-08-12 09:41:51 +0200

    chore: improve scripts

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-./zolad serve \
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if [ -h "$SCRIPT_PATH" ]; then
+  while [ -h "$SCRIPT_PATH" ] ; do cd "$(dirname "$SCRIPT_PATH")"; SCRIPT_PATH=$(readlink "$SCRIPT_PATH"); done
+fi
+cd "$(dirname "$SCRIPT_PATH")" > /dev/null
+SCRIPT_PATH="$(pwd)";
+popd  > /dev/null
+
+$SCRIPT_PATH/../zolad serve \
   --interface 0.0.0.0 \
   --base-url 0.0.0.0
